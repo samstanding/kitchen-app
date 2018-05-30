4a2fea80c123b35b31e20051e71001548b9ad8d6
Sam Standing <samzstanding@gmail.com>
2018-05-30 11:42:57 -0500

    trying to clear env file

--- src/components/KitchenForm.js
+++ src/components/KitchenForm.js
@@ -4,7 +4,6 @@ import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
